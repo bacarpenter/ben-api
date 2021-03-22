d7d95e511aac9f3761a28b9aa5e223bfddbb6c03
Ben Carpenter <bacarpenter04@gmail.com>
2021-03-22 13:50:38 -0400

    init commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 80;
+
+payload = {
+  first: "Ben",
+  last: "Carpenter",
+  contact: [4138341510, "bacarpenter04@gmail.com"],
+  pronouns: ["he", "him", "his"],
+};
+
+app.get("/", (req, res) => {
+  res.send(payload);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
