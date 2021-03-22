6bf5d69637cc7603614b782cdeacddf10d272229
Ben Carpenter <bacarpenter04@gmail.com>
2021-03-22 16:24:40 -0400

    Preped for Heroku

--- src/index.js
+++ src/index.js
@@ -14,5 +14,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log("ben-api listening at https://benc-api.herokuapp.com/");
 });
